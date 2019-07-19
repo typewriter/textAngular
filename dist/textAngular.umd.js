38e532f8116e96f2f087becf414f6f809e72e367
Yuto Segoe <yuto-segoe@m3.com>
2019-07-19 16:22:22 +0900

    Fixed problem that decodes text in first line

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -1982,6 +1982,13 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
 
 			var _ensureContentWrapped = function(value) {
 				if (_blankTest(value)) return value;
+
+				// wrap first line in default tag not to detect as text
+				value = value.replace(
+					new RegExp(`(.*?)(<${attrs.taDefaultWrap}>.*)`),
+					`<${attrs.taDefaultWrap}>$1</${attrs.taDefaultWrap}>$2`
+				);
+
 				var domTest = angular.element("<div>" + value + "</div>");
 				//console.log('domTest.children().length():', domTest.children().length);
 				if (domTest.children().length === 0) {
